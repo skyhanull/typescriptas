2d75f5c810b552eeb6ed90641247d23205653bf4
skyhanull <anky0425@naver.com>
2022-08-06 01:36:25 +0900

    오류

---
+++ src/guindex.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GuGuDan from "./GuGuDan";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <GuGuDan />
+  </React.StrictMode>
+);
+
